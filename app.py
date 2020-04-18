c573bb399249dc489651cf1292c23f487deb6d05
unknown <exgin3538@yahoo.com>
2020-04-18 15:52:37 -0400

    make models.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 """Flask app for Cupcakes"""
+from flask import Flask, request, jsonify, render_template
+from models import db, connect_db, Cupcake
+from secrets import APP_CONFIG_SECRET_KEY
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcake_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = APP_CONFIG_SECRET_KEY
+
+connect_db(app)
+
+
+@app.route('/')
+def home():
+    """Home page"""
+
+    return ":0"
